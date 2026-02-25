f2c210d8e99a7651ecc246b544200760e310f6f4
Kevin Stef De La Cruz Escate <sophiadelacruz123123@gmail.com>
2026-02-25 00:06:39 -0500

    NUEVO CAMBIO

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const client = new Client({
         dataPath: './session'
     }),
     puppeteer: {
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/opt/render/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome',
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
